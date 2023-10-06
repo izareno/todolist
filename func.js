91b99022da5df57a4fb3ae4ea550fc016b1dac4b
izareno <icrawford622@gmail.com>
2023-10-06 09:08:43 -0400

    added urg and imp

--- func.js
+++ func.js
@@ -1,6 +1,11 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 const dueDate = document.getElementById("dueDate");
+const urgency = document.getElementById("urgency");
+const importance = document.getElementById("importance");
+
+// add color change depending on the value of urg and imp
+
 
 function addTask(){
     if(inputBox.value === ''){
@@ -10,6 +15,12 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let urg = document.createElement("urg");
+        urg.innerHTML = urgency.value;
+        li.appendChild(urg);
+        let imp = document.createElement("imp");
+        imp.innerHTML = importance.value;
+        li.appendChild(imp);
         let dd = document.createElement("dd");
         dd.innerHTML = dueDate.value;
         li.appendChild(dd);
@@ -19,6 +30,12 @@ function addTask(){
     }
     inputBox.value = "";
 }
+
+// add subtasks
+
+
+
+// add celebration effect
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI") {
